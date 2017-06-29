2c0bf5aa27124afbcf78095be95114ddc4bda611
Yen <yenchieh86@hotmail.com>
2017-06-29 21:58:41 +0000

    Separate different part of properties and values

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,14 @@ class HomeController < ApplicationController
   end
   
   def search
-    @user = check_user(params[:q])
+    info = check_user(params[:q])
+    
+    if info.include?('"message":"Validation Failed"') || info.include?('"total_count":0')
+      @user = 0
+    else
+      @user = single_user_info(info)
+    end
+    
     respond_to do |format|
       format.js
     end
@@ -13,7 +20,21 @@ class HomeController < ApplicationController
   
     def check_user(username)
       uri = URI('https://api.github.com/search/users?q=' + username)
-      @result = Net::HTTP.get(uri)
+      result = Net::HTTP.get(uri)
     end
-
+    
+    def single_user_info(info)
+      userdata = info.slice(/"login".*?"score".*?}/).chomp('}').split(',')
+      
+      user = Hash.new
+      properties = ['login', 'id', 'avatar_url', 'gravatar_id', 'url',
+             'html_url', 'followers_url', 'following_url', 'gists_url',
+             'starred_url', 'subscriptions_url', 'organizations_url',
+             'repos_url', 'events_url', 'received_events_url', 
+             'type', 'site_admin', 'score']
+      
+      userdata.each_with_index{ |x, i| user[properties[i]] = (x.split('":')[1]) }
+      user.map{ |x| x[1].delete!('"')}
+      user
+    end
 end
